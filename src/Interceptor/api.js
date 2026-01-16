0e98b47cf88a3619282b0fd8ac40a9a1d2ef8688
Shubham Rajesh Saroj <57361979+shubhamrsaroj@users.noreply.github.com>
2026-01-16 13:24:14 +0530

    added config in api.js

--- src/Interceptor/api.js
+++ src/Interceptor/api.js
@@ -2,17 +2,17 @@ import axios from "axios";
 
 
 export const api = axios.create({
-    baseURL:"http://localhost:5000/api",
-    headers:{
-        "Content-Type":"application/json"
+    baseURL: ["http://localhost:5000/api", "https://instagram-clone-df6g.onrender.com"],
+    headers: {
+        "Content-Type": "application/json"
     }
 })
 
-api.interceptors.request.use((config)=>{
-    
+api.interceptors.request.use((config) => {
+
     const token = localStorage.getItem("token");
 
-    if(token){
+    if (token) {
 
         config.headers.Authorization = `Bearer ${token}`;
 
@@ -23,16 +23,16 @@ api.interceptors.request.use((config)=>{
 });
 
 
-api.interceptors.response.use((res)=>res,(error)=>{
-    
-    if(error.response.status === 401){
-        
+api.interceptors.response.use((res) => res, (error) => {
+
+    if (error.response.status === 401) {
+
         localStorage.removeItem("token");
-    
-        window.location.href="/login";
+
+        window.location.href = "/login";
     }
 
     return Promise.reject(error);
-    
+
 
 })
