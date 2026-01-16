c6d2fe19a6624ea59ac0add2eafa14e469cbc9a4
Shubham Rajesh Saroj <57361979+shubhamrsaroj@users.noreply.github.com>
2026-01-16 13:20:16 +0530

    latest config in server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,14 @@ dotenv.config();
 
 const apis = express();
 
-apis.use(cors({origin:"http://localhost:3000"}));
+apis.use(
+    cors({
+        origin: [
+            "http://localhost:3000",
+            "https://instacloclo.netlify.app"
+        ]
+    })
+);
 
 apis.use(express.json());
 
@@ -19,15 +26,15 @@ const files = fileURLToPath(import.meta.url);
 
 const dirname = path.dirname(files);
 
-apis.use("/uploads",express.static(path.join(dirname,"uploads")));
+apis.use("/uploads", express.static(path.join(dirname, "uploads")));
 
-apis.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
+apis.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 
-apis.use("/api/auth",routers);
+apis.use("/api/auth", routers);
 
 mongoose.connect(process.env.MONGODB_URI)
-.then(()=>{console.log("mongodb connected")});
+    .then(() => { console.log("mongodb connected") });
 
-apis.listen(5000,()=>{
+apis.listen(5000, () => {
     console.log("Server Started Successfully");
 })
